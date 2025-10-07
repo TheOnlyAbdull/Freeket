78a7cb138ceb1286e115e13f7b4ff8fd0fc3fef5
Abdullahi Salaudeen <abdullahisalaudeen65@gmail.com>
2025-10-07 15:10:19 +0100

    Edit sign up UI

--- src/features/authentication/SignUp.jsx
+++ src/features/authentication/SignUp.jsx
@@ -28,119 +28,117 @@ function SignUp({ setCreateAccount }) {
     setEmail("");
     setPassword("");
   }
-  return (
-    <>
-      <h1 className="text-3xl mb-1 font-bold">Request Account Access</h1>
-      <p className="mb-6 text-gray-500">
-        To create an account, please send a request for permission.
-      </p>
-      <a
-        href="mailto:abdullahisalaudeen065@gmail.com?subject=Request%20for%20Account%20Access%20to%20FREEKET&body=Hi,%0AI%20would%20like%20to%20request%20permission%20to%20sign%20up.%0AThank%20you."
-        className="my-6 cursor-pointer rounded-sm bg-orange-500 text-white w-full py-3 font-bold block text-center"
-      >
-        Request Permission via Email
-      </a>
-      <div className="text-sm text-gray-500 mt-4">
-        <p>
-          Already have permission?{" "}
-          <span
-            className="text-cyan-400 cursor-pointer"
-            onClick={() => setCreateAccount(false)}
-          >
-            Log in instead
-          </span>
-        </p>
-      </div>
-    </>
-  );
-
-
-//Sign in without
   // return (
   //   <>
-  //     <h1 className="text-3xl mb-1 font-bold">Create account</</form>h1>
-  //     <p className="mb-6 text-gray-</div>500">
-  //       Enter your details to create your account
+  //     <h1 className="text-3xl mb-1 font-bold">Request Account Access</h1>
+  //     <p className="mb-6 text-gray-500">
+  //       To create an account, please send a request for permission.
   //     </p>
-
-  //     <form onSubmit={handleSignUp}>
-  //       <div className="flex md:flex-row gap-4">
-  //         <div className="w-6/12">
-  //           <label htmlFor="first-name">First Name</label>
-  //           <input
-  //             className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
-  //             placeholder="Enter First Name"
-  //             type="text"
-  //             id="first-name"
-  //             name="first-name"
-  //             required
-  //             value={firstName}
-  //             onChange={(e) => setFirstName(e.target.value)}
-  //           />
-  //         </div>
-  //         <div className="w-6/12">
-  //           <label htmlFor="last-name">Last Name</label>
-  //           <input
-  //             className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
-  //             placeholder="Enter Last Name"
-  //             type="text"
-  //             id="last-name"
-  //             name="last-name"
-  //             required
-  //             value={lastName}
-  //             onChange={(e) => setLastName(e.target.value)}
-  //           />
-  //         </div>
-  //       </div>
-
-  //       <div>
-  //         <label htmlFor="email">Email address</label>
-  //         <input
-  //           className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
-  //           placeholder="Enter email address"
-  //           type="email"
-  //           id="email"
-  //           name="email"
-  //           required
-  //           value={email}
-  //           onChange={(e) => setEmail(e.target.value)}
-  //         />
-  //       </div>
-  //       <div>
-  //         <label htmlFor="password">Password</label>
-  //         <input
-  //           className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
-  //           placeholder="Enter Password, minimum 8 characters"
-  //           type="password"
-  //           id="password"
-  //           name="password"
-  //           required
-  //           minLength={8}
-  //           value={password}
-  //           onChange={(e) => setPassword(e.target.value)}
-  //         />
-  //       </div>
-  //       <button
-  //         className="my-6 cursor-pointer rounded-sm bg-orange-500 text-white w-full py-3 font-bold block text-center"
-  //         // tpe="submit"
-  //         onClick={handleSignUp}
-  //       >
-  //         Create Account
-  //       </button>
-  //     </form>
-  //     <div className="text-sm  text-gray-500">
+  //     <a
+  //       href="mailto:abdullahisalaudeen065@gmail.com?subject=Request%20for%20Account%20Access%20to%20FREEKET&body=Hi,%0AI%20would%20like%20to%20request%20permission%20to%20sign%20up.%0AThank%20you."
+  //       className="my-6 cursor-pointer rounded-sm bg-orange-500 text-white w-full py-3 font-bold block text-center"
+  //     >
+  //       Request Permission via Email
+  //     </a>
+  //     <div className="text-sm text-gray-500 mt-4">
   //       <p>
-  //         Have an account?{""}
+  //         Already have permission?{" "}
   //         <span
   //           className="text-cyan-400 cursor-pointer"
   //           onClick={() => setCreateAccount(false)}
   //         >
-  //           Log in intead
+  //           Log in instead
   //         </span>
   //       </p>
   //     </div>
   //   </>
   // );
+
+  return (
+    <>
+      <h1 className="text-3xl mb-1 font-bold">Create account</h1>
+      <p className="mb-6 text-gray-500">
+        Enter your details to create your account
+      </p>
+
+      <form onSubmit={handleSignUp}>
+        <div className="flex md:flex-row gap-4">
+          <div className="w-6/12">
+            <label htmlFor="first-name">First Name</label>
+            <input
+              className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
+              placeholder="Enter First Name"
+              type="text"
+              id="first-name"
+              name="first-name"
+              required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="w-6/12">
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
+              placeholder="Enter Last Name"
+              type="text"
+              id="last-name"
+              name="last-name"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email address</label>
+          <input
+            className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
+            placeholder="Enter email address"
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            className="block outline-1 text-gray-500 w-full mb-4 rounded-sm p-4 text-sm"
+            placeholder="Enter Password, minimum 8 characters"
+            type="password"
+            id="password"
+            name="password"
+            required
+            minLength={8}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          className="my-6 cursor-pointer rounded-sm bg-orange-500 text-white w-full py-3 font-bold block text-center"
+          type="submit"
+          onClick={handleSignUp}
+        >
+          Create Account
+        </button>
+      </form>
+      <div className="text-sm  text-gray-500">
+        <p>
+          Have an account?{""}
+          <span
+            className="text-cyan-400 cursor-pointer"
+            onClick={() => setCreateAccount(false)}
+          >
+            Log in intead
+          </span>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
